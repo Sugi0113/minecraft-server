e2d8269cb2ebd4ab5870ab7474925b55d81bc3a9
root <root@instance-1.us-west1-b.c.minecraft-316200.internal>
2021-06-09 10:00:21 +0000

    シェルスクリプトのミスを直した

--- start-controll.sh
+++ start-controll.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while:; do node /home/sugi/git/minecraft-server/controll-server.js >>/home/sugi/git/minecraft-server/log/node 2>>/home/sugi/git/minecraft-server/err/node done
+while:; do node /home/sugi/git/minecraft-server/controll-server.js >>/home/sugi/git/minecraft-server/log/node 2>>/home/sugi/git/minecraft-server/err/node; done
